9e6d59b37f7ba56e377ca23c15bf2988e650a936
CodeWithTamim <tamimh.dev@gmail.com>
2024-12-22 18:57:54 +0600

    Fix
    Fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.nasahacker.nasaeditor"
         minSdk = 21
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.0.5-beta"
+        versionCode = 7
+        versionName = "1.0.6-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
