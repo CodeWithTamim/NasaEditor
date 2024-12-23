6eb221e97abb35dfa632acff681a2e47353d1360
CodeWithTamim <tamimh.dev@gmail.com>
2024-12-23 09:13:58 +0600

    Fix the dumb issue
    Fix the dumb issue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.nasahacker.nasaeditor"
         minSdk = 21
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.0.8-beta"
+        versionCode = 10
+        versionName = "1.0.9-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
